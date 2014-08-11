79836e5e7c471a6d85247a91a7ec9a25be4662c1
Alexander Mertens <alexander.mertens@me.com>
2014-08-11 20:26:21 +0200

    script is more flexible. first argument is backuptype (inc for incremental/differntial backup, profile for the profile directory located in the tartarus config base dir also added some parameter tests

--- bin/tartarusbackup.sh
+++ bin/tartarusbackup.sh
@@ -1,8 +1,34 @@
 #!/bin/sh
-# /usr/local/sbin/backup.sh
-# Run all backup profile found in /etc/tartarus/ and pass
-# command line arguments on to tartarus (e.g. -i)
-# source: http://wiki.hetzner.de/index.php/Tartarus_Backup-Konfiguration
-for profile in /etc/tartarus/*.conf; do
-  /usr/sbin/tartarus $* "$profile"
+# /usr/local/sbin/tartarusbackup.sh
+# Run all backup configs wtih specified profile directory under the base dir
+# based on: http://wiki.hetzner.de/index.php/Tartarus_Backup-Konfiguration
+
+PROFILEBASEDIR="/etc/tartarus"
+TARTARUSBIN="/usr/sbin/tartarus"
+
+# parameter count must equals two ($1 = backuptype, $2 = profile directory)
+if [ $# -ne 2 ]; then
+  echo "usage: $0 full|inc profile"
+  exit 1
+fi
+
+# set profile directory and test if exists and accessable
+PROFILEDIR="$PROFILEBASEDIR/${2}"
+if [ ! -d "$PROFILEDIR" ]; then
+  echo "profile directory isn't accessable or doesn't exist!"
+  exit 1
+fi
+
+# execute tartarus with every configfile in profile directory
+for config in $PROFILEDIR/*.conf; do
+
+  # test if fill or incremental backup shall performed 
+  if [ "$1" = "inc" -o "$1" = "i" -o "$1" = "-i" ]; then
+    $TARTARUSBIN -i "$config"
+  else
+    $TARTARUSBIN "$config"
+  fi
+
 done
+
+exit 0
